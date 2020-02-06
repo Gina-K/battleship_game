58d1f59af6ee54f8d860fff9c8b27f493e39d2ec
Gina <e.kurmeneva@gmail.com>
2020-02-06 14:24:24 +0300

    Declare variables
    Set up a ship location manually just for the start. I'll write the code
    to generate random location later.

---
+++ battleship.js
@@ -0,0 +1,41 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+
+
+// I've defined the ship location random by myself. Delete when do it by book
+
+/*
+var location1 = getRandomInt(7);
+var location2;
+var location3;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+if (location1 < 6) {
+    location2 = location1 + 1;
+} else {
+    location2 = location1 - 1;
+}
+
+if (location2 > location1) {
+    if (location2 < 6) {
+        location3 = location2 + 1;
+    } else {
+        location3 = location1-1
+    }
+} else {
+    location3 = location2 - 1;
+}
+
+
+console.log(location1);
+console.log(location2);
+console.log(location3);*/
