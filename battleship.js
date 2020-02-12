1aac36e437816c86a4fb9dc99d3b6ff62497f669
Gina <e.kurmeneva@gmail.com>
2020-02-12 08:20:45 +0300

    Add random location of the ship

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random()*5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -28,36 +29,3 @@ while (isSunk == false) {
 
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
 alert(stats);
-
-
-// I've defined the ship location random by myself. Delete when do it by book
-
-/*
-var location1 = getRandomInt(7);
-var location2;
-var location3;
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-if (location1 < 6) {
-    location2 = location1 + 1;
-} else {
-    location2 = location1 - 1;
-}
-
-if (location2 > location1) {
-    if (location2 < 6) {
-        location3 = location2 + 1;
-    } else {
-        location3 = location1-1
-    }
-} else {
-    location3 = location2 - 1;
-}
-
-
-console.log(location1);
-console.log(location2);
-console.log(location3);*/
